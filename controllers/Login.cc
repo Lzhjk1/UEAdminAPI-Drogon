298dad1a755f182b42963b585409d413891a8574
Roasal <everspace@qq.com>
2025-11-24 17:56:29 +0800

    登录接口修改

--- controllers/Login.cc
+++ controllers/Login.cc
@@ -106,10 +106,12 @@ Task<HttpResponsePtr> Login::LoginByOther(HttpRequestPtr req, std::string target
     auto _mfaService = MFAService::Instance();
     auto _authService = AuthService::Instance();
 
+    // 用于返回结果的相关变量预定义
     auto resp = HttpResponse::newHttpResponse();
     resp->setStatusCode(k200OK);
     HttpResult result;
 
+    // 检查验证码
     auto [isSuccess, errMsg] = co_await _mfaService->VerifyTheCode(target, verifyCode, eMFAType::Login);
     if(!isSuccess) {
         LOG_ERROR << errMsg;
@@ -118,15 +120,41 @@ Task<HttpResponsePtr> Login::LoginByOther(HttpRequestPtr req, std::string target
         co_return resp;
     }
 
+    // 获取dbClient并创建mapper, 固定用法
     auto dbClientPtr = drogon::app().getDbClient();
     drogon::orm::Mapper<User> mapperUsers(dbClientPtr);
-    auto targetUser = mapperUsers.findFutureOne(Criteria(targetDBColName, CompareOperator::EQ, target)).get();
+    drogon::orm::Mapper<UserFlashtoken> mapperFlashToken(dbClientPtr);
+    
+    // 查找用户
+    User targetUser;
+    try{
+        targetUser = mapperUsers.findFutureOne(Criteria(targetDBColName, CompareOperator::EQ, target)).get();
+    }
+    // 没找到会抛出异常
+    catch (UnexpectedRows& e){
+        LOG_ERROR << "邮箱验证码登录查询用户时出错!";
+        result.setResult(-1, "用户不存在");
+        resp->setBody(result.toJsonString());
+        co_return resp;
+    }
 
+    // 根据刚找到的用户的关系从数据库获取对应的user_flash_token的记录
     auto flashtoken = targetUser.getUser_flashtoken(dbClientPtr);
-    LOG_ERROR << flashtoken.getValueOfStatus();
-
+    
+    // 创建新的FlashToken
+    // 生成随机数作为status, status用于标记最新的token 
+    //(虽说这样是抛弃了JWT无状态的优点, 但也只是存了一个int数, 开销不大, 免去了要处理同时有多个FlashToken可用的问题的麻烦)
     int status = RandomGenerator::getInt(1, INT_MAX);
     string token = _authService->CreateFlashToken(targetUser.getValueOfId(), status);
+    // 更新状态到数据库
+    flashtoken.setStatus(status);
+    auto ret = mapperFlashToken.update(flashtoken);
+    if (ret != 1) {
+        result.setResult(-1, "更新状态失败");
+        resp->setStatusCode(k500InternalServerError);
+        resp->setBody(result.toJsonString());
+        co_return resp;
+    }
 
     result.jsondata["FlashToken"] = token;
     result.setResult(0, "登录成功");
