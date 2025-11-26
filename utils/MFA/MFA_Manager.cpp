3c698ba37586786d8edd631d575a79ecd9004d5f
Roasal <everspace@qq.com>
2025-11-26 16:01:24 +0800

    没做什么修改, 只是调整了缩进

--- utils/MFA/MFA_Manager.cpp
+++ utils/MFA/MFA_Manager.cpp
@@ -1,57 +1,56 @@
 #include "MFA_Manager.h"
 
-MFA_Manager::MFA_Manager(IEmailService *emailService, ISmsService *smsService)
-{
-  // 添加渠道
-  _channels.push_back(std::make_shared<MFA_SMSChannel>(smsService));
-  _channels.push_back(std::make_shared<MFA_EmailChannel>(emailService));
+MFA_Manager::MFA_Manager(IEmailService *emailService, ISmsService *smsService) {
+    // 添加渠道
+    _channels.push_back(std::make_shared<MFA_SMSChannel>(smsService));
+    _channels.push_back(std::make_shared<MFA_EmailChannel>(emailService));
 }
 
 std::shared_ptr<IMFAChannel> MFA_Manager::GetChannel(eChannelType type) {
-  std::lock_guard lock(_mutex);
-  for (const auto& ch : _channels) {
-      if (ch->ChannelType() == type)
-          return ch;
-  }
-  return nullptr;
+    std::lock_guard lock(_mutex);
+    for (const auto &ch : _channels) {
+        if (ch->ChannelType() == type)
+            return ch;
+    }
+    return nullptr;
 }
 
-std::pair<bool, std::string> MFA_Manager::VerifyTheCode(const std::string& target, const std::string& code, eMFAType type) {
-  auto channelType = MFAChannelBase::DetermineChannelType(target);
-  if (channelType == eChannelType::None) {
-      return {false, "无法判断渠道类型"};
-  }
-  auto channel = GetChannel(channelType);
-  if (!channel) {
-      return {false, "无法找到对应的验证码发送渠道"};
-  }
-  std::string errorMsg;
-  bool result = channel->VerifyTheCode(target, code, type, errorMsg);
-  return {result, errorMsg};
+std::pair<bool, std::string> MFA_Manager::VerifyTheCode(const std::string &target, const std::string &code, eMFAType type) {
+    auto channelType = MFAChannelBase::DetermineChannelType(target);
+    if (channelType == eChannelType::None) {
+        return {false, "无法判断渠道类型"};
+    }
+    auto channel = GetChannel(channelType);
+    if (!channel) {
+        return {false, "无法找到对应的验证码发送渠道"};
+    }
+    std::string errorMsg;
+    bool result = channel->VerifyTheCode(target, code, type, errorMsg);
+    return {result, errorMsg};
 }
 
-drogon::Task<std::pair<bool, std::string>> MFA_Manager::SendTheCode(const std::string& target, const std::string& code, eMFAType type) {
+drogon::Task<std::pair<bool, std::string>> MFA_Manager::SendTheCode(const std::string &target, const std::string &code, eMFAType type) {
     if (type == eMFAType::Error) {
         co_return {false, "验证码类型错误"};
     }
 
-  auto channelType = MFAChannelBase::DetermineChannelType(target);
-  if (channelType == eChannelType::None) {
-      co_return {false, "无法判断渠道类型"};
-  }
-  auto channel = GetChannel(channelType);
-  if (!channel) {
-      co_return {false, "无法找到对应的验证码发送渠道"};
-  }
-  std::string innerErrorMsg;
-  auto codePair = CodePairBase::CreateCodePair(target, code, type, innerErrorMsg);
-  if (!codePair) {
-      LOG_ERROR << format("验证码发送失败, 生成CodePair失败 : {}", innerErrorMsg);
-      co_return {false, innerErrorMsg};
-  }
-  auto [result, errorMsg] = co_await channel->SendCode(codePair);
-  if (!result) {
-      LOG_ERROR << "验证码发送失败";
-  }
-  co_return {result, errorMsg};
+    auto channelType = MFAChannelBase::DetermineChannelType(target);
+    if (channelType == eChannelType::None) {
+        co_return {false, "无法判断渠道类型"};
+    }
+    auto channel = GetChannel(channelType);
+    if (!channel) {
+        co_return {false, "无法找到对应的验证码发送渠道"};
+    }
+    std::string innerErrorMsg;
+    auto codePair = CodePairBase::CreateCodePair(target, code, type, innerErrorMsg);
+    if (!codePair) {
+        LOG_ERROR << format("验证码发送失败, 生成CodePair失败 : {}", innerErrorMsg);
+        co_return {false, innerErrorMsg};
+    }
+    auto [result, errorMsg] = co_await channel->SendCode(codePair);
+    if (!result) {
+        LOG_ERROR << "验证码发送失败";
+    }
+    co_return {result, errorMsg};
 }
--- utils/MFA/MFA_Manager.h
+++ utils/MFA/MFA_Manager.h
@@ -1,24 +1,26 @@
 #pragma once
-#include <drogon/drogon.h>
-#include <vector>
-#include <memory>
-#include <mutex>
-#include "MFA_Channels.h"
 #include "MFACodePair.h"
+#include "MFA_Channels.h"
 #include "utils/Singleton.h"
 #include "utils/SingletonWithInit.h"
+#include <chrono>
+#include <drogon/drogon.h>
+#include <memory>
+#include <mutex>
+#include <vector>
 
-class MFA_Manager{
-  std::mutex _mutex;
-  std::vector<std::shared_ptr<IMFAChannel>> _channels;
+class MFA_Manager {
+    std::mutex _mutex;
+    std::vector<std::shared_ptr<IMFAChannel>> _channels;
 
 public:
-  MFA_Manager(IEmailService *emailService, ISmsService *smsService);
+    MFA_Manager(IEmailService *emailService, ISmsService *smsService);
 
-  std::shared_ptr<IMFAChannel> GetChannel(eChannelType type);
+    std::shared_ptr<IMFAChannel> GetChannel(eChannelType type);
 
-  std::pair<bool, std::string> VerifyTheCode(const std::string& target, const std::string& code, eMFAType type);
+    std::pair<bool, std::string> VerifyTheCode(const std::string &target, const std::string &code, eMFAType type);
 
-  // code留空时自动生成(不可为null)
-  drogon::Task<std::pair<bool, std::string>> SendTheCode(const std::string& target, const std::string& code, eMFAType type);
+    // code留空时自动生成(不可为null)
+    drogon::Task<std::pair<bool, std::string>> SendTheCode(const std::string &target, const std::string &code, eMFAType type);
+    
 };
