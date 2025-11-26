5e0c68049250a8cf9599076bdb3e79b10f1c3456
Roasal <everspace@qq.com>
2025-11-26 15:55:39 +0800

    验证码发送接口增加冷却时间

--- controllers/SendVerifyCode.cc
+++ controllers/SendVerifyCode.cc
@@ -7,11 +7,20 @@ using namespace UEAdminAPI;
 using namespace UEAdminAPI::utils;
 
 Task<HttpResponsePtr> SendVerifyCode::SendCode(HttpRequestPtr req, std::string target, std::string type) {
-	auto resp = HttpResponse::newHttpResponse();
-    auto mfaService = MFAService::Instance();
-    auto [isSuccess, errMsg] = co_await mfaService->SendTheCode(target, stringToMFAType(type));
+    // 依赖
+    auto _mfaService = MFAService::Instance();
 
+	auto resp = HttpResponse::newHttpResponse();
     HttpResult result;
+
+    // 检查IP是否处于冷却
+    if(IsInColdDown(req->getPeerAddr().toIp())) {    
+        result.setResult(-2, "发送过于频繁, 请稍后再试");
+        resp->setBody(result.toJsonString());
+        co_return resp;
+    }
+
+    auto [isSuccess, errMsg] = co_await _mfaService->SendTheCode(target, stringToMFAType(type));
 
     result.code = isSuccess ? 0 : -1;
     if(!isSuccess) {
@@ -20,4 +29,37 @@ Task<HttpResponsePtr> SendVerifyCode::SendCode(HttpRequestPtr req, std::string t
     
     resp->setBody(result.toJsonString());
     co_return resp;
+}
+
+bool SendVerifyCode::IsInColdDown(std::string ipAddr) {
+    std::lock_guard<std::mutex> lock(_mutexForColdDownMap);
+
+    bool isInColdDown = true;
+
+    auto it = _coldDownMap.find(ipAddr);
+    // 没找到记录, 就增加这一条记录并返回false, 即没有处于冷却中
+    if(it == _coldDownMap.end()) {
+        _coldDownMap[ipAddr] = std::chrono::system_clock::now();
+        isInColdDown = false;
+    }
+    // 找到了记录, 检查是否处于冷却中
+    else if(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - it->second).count() < _coldDownTime) {
+        isInColdDown =  true;
+    }
+    else{
+        _coldDownMap[ipAddr] = std::chrono::system_clock::now();
+        isInColdDown =  false;
+    }
+
+    // 清除过期的记录
+    for(auto it = _coldDownMap.begin(); it != _coldDownMap.end();) {
+        if(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - it->second).count() > _coldDownTime) {
+            it = _coldDownMap.erase(it);
+        }
+        else {
+            it++;
+        }
+    }
+
+    return isInColdDown;
 }
--- controllers/SendVerifyCode.h
+++ controllers/SendVerifyCode.h
@@ -3,23 +3,34 @@
 #include <drogon/HttpController.h>
 #include <drogon/utils/coroutine.h>
 #include "services/MFAService.h"
+#include <chrono>
 
 using namespace drogon;
 
 class SendVerifyCode : public drogon::HttpController<SendVerifyCode>
 {
-  public:
+public:
     METHOD_LIST_BEGIN
     // use METHOD_ADD to add your custom processing function here;
     // METHOD_ADD(SendVerifyCode::get, "/{2}/{1}", Get); // path is /SendVerifyCode/{arg2}/{arg1}
     // METHOD_ADD(SendVerifyCode::your_method_name, "/{1}/{2}/list", Get); // path is /SendVerifyCode/{arg1}/{arg2}/list
     // ADD_METHOD_TO(SendVerifyCode::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
-    ADD_METHOD_TO(SendVerifyCode::SendCode, "/SendVerifyCode?target={1}&type={2}", Post); // path is /SendVerifyCode/{arg1}/{arg2}
+    ADD_METHOD_TO(SendVerifyCode::SendCode, "/SendVerifyCode?target={1}&type={2}", Get); // path is /SendVerifyCode/{arg1}/{arg2}
 
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
     // void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, double p1, int p2) const;
 
+    // 发送验证码, 支持邮箱和手机号, 自动识别类型, 有冷却时间, 
+    // 因冷却时间导致的错误, 错误码为-2
     Task<HttpResponsePtr> SendCode(HttpRequestPtr req, std::string target, std::string type);
+
+    // 基于IP地址的冷却, 目前服务器是通过frp穿透来测试的, 所以IP是固定的, 所以冷却时间目前设置很短
+    bool IsInColdDown(std::string ipAddr);
+
+private:
+    std::mutex _mutexForColdDownMap;
+    int _coldDownTime = 2; // 冷却时间
+    std::unordered_map<std::string, std::chrono::system_clock::time_point> _coldDownMap;
 };
