84afdea39d1c9cf275b841a0266961dbc528694f
Roasal <everspace@qq.com>
2026-05-12 09:34:06 +0800

    编译脚本

--- cmake_sh.sh
+++ cmake_sh.sh
@@ -1,2 +1,8 @@
 cmake . \
-	-DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake
+	-DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake \
+	-G Ninja -B build \
+  	-DCMAKE_C_COMPILER=gcc-16 \
+  	-DCMAKE_CXX_COMPILER=g++-16
+
+echo "cmake done"
+echo "then run 'cmake --build build' to build"
