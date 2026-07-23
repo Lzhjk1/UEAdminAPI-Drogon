91deb2afd344330ebd4970ae2b3037cc754b121c
Roasal <everspace@qq.com>
2026-07-23 08:40:30 +0800

    清理项目目录

--- cmake_sh.sh
+++
@@ -1,9 +0,0 @@
-cmake . \
-	-DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake \
-	-G Ninja -B build \
-	-DCMAKE_BUILD_TYPE=Release \
-  	-DCMAKE_C_COMPILER=gcc-16 \
-  	-DCMAKE_CXX_COMPILER=g++-16
-
-echo "cmake done"
-echo "then run 'cmake --build build' to build"
